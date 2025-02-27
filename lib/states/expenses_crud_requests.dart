cc43568b4bedfdbb70c3b060d2cc3c1dfff44e5a
Ahmed Gamil Fathy <rabiahmed2001@yahoo.com>
2025-02-28 00:04:35 +0200

    request

---
+++ lib/states/expenses_crud_requests.dart
@@ -0,0 +1,75 @@
+import 'package:budgeting_app/main.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../models/expense_model.dart';
+
+part 'expenses_crud_requests.freezed.dart';
+
+class ExpensesCrudRequestsCubit extends Cubit<ExpenseRequestState> {
+  ExpensesCrudRequestsCubit() : super(ExpenseRequestState.initial()) {
+    _fetchExpenses();
+  }
+
+  Future<void> updateExpenses(Expense expense) async {
+    emit(state.copyWith(status: ExpenseRequestStatus.loading));
+    try {
+      await dbManager.updateExpense(expense);
+      emit(state.copyWith(status: ExpenseRequestStatus.success));
+    } catch (e) {
+      emit(state.copyWith(status: ExpenseRequestStatus.error));
+    }
+    await _fetchExpenses();
+  }
+
+  Future<void> insertExpense(Expense expense) async {
+    emit(state.copyWith(status: ExpenseRequestStatus.loading));
+    try {
+      await dbManager.insertExpense(expense);
+      emit(state.copyWith(status: ExpenseRequestStatus.success));
+    } catch (e) {
+      emit(state.copyWith(status: ExpenseRequestStatus.error));
+    }
+    await _fetchExpenses();
+  }
+
+  Future<void> deleteExpense(int id) async {
+    emit(state.copyWith(status: ExpenseRequestStatus.loading));
+    try {
+      await dbManager.deleteExpense(id);
+      emit(state.copyWith(status: ExpenseRequestStatus.success));
+    } catch (e) {
+      emit(state.copyWith(status: ExpenseRequestStatus.error));
+    }
+    await _fetchExpenses();
+  }
+
+  Future<void> _fetchExpenses() async {
+    emit(state.copyWith(status: ExpenseRequestStatus.loading));
+
+    try {
+      final expenses = await dbManager.getExpenses();
+      emit(
+        state.copyWith(
+          status: ExpenseRequestStatus.success,
+          expenses: expenses,
+        ),
+      );
+    } catch (e) {
+      emit(state.copyWith(status: ExpenseRequestStatus.error));
+    }
+  }
+}
+
+@freezed
+abstract class ExpenseRequestState with _$ExpenseRequestState {
+  const factory ExpenseRequestState({
+    required ExpenseRequestStatus status,
+    required List<Expense> expenses,
+  }) = _ExpenseRequestState;
+
+  factory ExpenseRequestState.initial() =>
+      ExpenseRequestState(status: ExpenseRequestStatus.initial, expenses: []);
+}
+
+enum ExpenseRequestStatus { initial, loading, success, error }
